3823ec8ae05271c450bfeac7a7e2ad7409576f02
shy19940918@live.com <mn814561293>
2018-09-12 15:35:48 +0800

    完善log4j2配置

--- src/main/java/com/pcbwx/jsp/config/Log4j2Config.java
+++ src/main/java/com/pcbwx/jsp/config/Log4j2Config.java
@@ -1,5 +1,6 @@
 package com.pcbwx.jsp.config;
 
+import com.pcbwx.jsp.SystemStart;
 import com.pcbwx.jsp.common.ConfigProperties;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.LoggerContext;
@@ -13,8 +14,6 @@ import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
 import org.apache.logging.log4j.core.config.builder.api.LoggerComponentBuilder;
 import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.Ordered;
 
 import java.net.URI;
@@ -22,10 +21,20 @@ import java.net.URI;
 /**
  * @author heyu
  */
-@Plugin(name = "CustomConfigurationFactory", category = ConfigurationFactory.CATEGORY)
+@Plugin(name = "log4j2Config", category = ConfigurationFactory.CATEGORY)
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @org.springframework.context.annotation.Configuration
-public class CustomConfigurationFactory extends ConfigurationFactory{
+public class Log4j2Config extends ConfigurationFactory{
+
+    private static final String INFOAPPENDER = "myInfo";
+
+    private static final String ERRORAPPENDER = "myError";
+
+    private static final String CONSOLE = "Console";
+
+    private static final String ASYNC = "ASYNC";
+
+    private static final String FILEPATH = "/opt/logs/" + SystemStart.SYSTEM;
 
 	private Configuration createConfiguration(final String name, ConfigurationBuilder<BuiltConfiguration> builder) {
         builder.setConfigurationName(name);
@@ -33,19 +42,14 @@ public class CustomConfigurationFactory extends ConfigurationFactory{
         if (ConfigProperties.getDebugLog()) {
             builder.add(getConsoleBuilder(builder));
             addDevLogger(builder);
-            builder.add(builder.newRootLogger(Level.INFO).add(builder.newAppenderRef("Console")));
+            builder.add(builder.newRootLogger(Level.INFO).add(builder.newAppenderRef(CONSOLE)));
         } else {
             builder.add(getErrorFileBuilder(builder));
             builder.add(getInfoFileBuilder(builder));
             builder.add(getAsyncBuilder(builder));
-            builder.add(builder.newRootLogger(Level.INFO).add(builder.newAppenderRef("ASYNC")));
+            builder.add(builder.newRootLogger(Level.INFO).add(builder.newAppenderRef(ASYNC)));
         }
         return builder.build();
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
     }
 
     @Override
@@ -72,7 +76,7 @@ public class CustomConfigurationFactory extends ConfigurationFactory{
 	private AppenderComponentBuilder getConsoleBuilder(
 			ConfigurationBuilder<BuiltConfiguration> builder) {
 		AppenderComponentBuilder appenderBuilder = builder.newAppender(
-				"Console", "Console").addAttribute("target",
+                CONSOLE, "Console").addAttribute("target",
 				ConsoleAppender.Target.SYSTEM_OUT);
 		appenderBuilder.add(builder.newLayout("PatternLayout").addAttribute(
 				"pattern",
@@ -85,14 +89,14 @@ public class CustomConfigurationFactory extends ConfigurationFactory{
 	 * @param builder
 	 * @return
 	 */
-	private AppenderComponentBuilder getErrorFileBuilder(
+	private AppenderComponentBuilder getInfoFileBuilder(
 			ConfigurationBuilder<BuiltConfiguration> builder) {
 		AppenderComponentBuilder appenderBuilder = builder
-				.newAppender("myInfo", "RollingFile")
-				.addAttribute("fileName", "/opt/logs/jsp/info/info.log")
-				.addAttribute("filePattern", "/opt/logs/jsp/info/info-%d{yyyy-MM-dd}.log");
+				.newAppender(INFOAPPENDER, "RollingFile")
+				.addAttribute("fileName", FILEPATH + "/info/info.log")
+				.addAttribute("filePattern", FILEPATH + "/info/info-%d{yyyy-MM-dd}.log");
 		appenderBuilder.add(builder.newLayout("ThresholdFilter")
-				.addAttribute("level", "INFO")
+				.addAttribute("level", Level.INFO)
 				.addAttribute("onMatch", "ACCEPT")
 				.addAttribute("onMismatch", "DENY"));
 		appenderBuilder.add(builder.newLayout("PatternLayout").addAttribute(
@@ -110,16 +114,16 @@ public class CustomConfigurationFactory extends ConfigurationFactory{
 	 * @param builder
 	 * @return
 	 */
-	private AppenderComponentBuilder getInfoFileBuilder(
+	private AppenderComponentBuilder getErrorFileBuilder(
 			ConfigurationBuilder<BuiltConfiguration> builder) {
 		AppenderComponentBuilder appenderBuilder = builder
-				.newAppender("myError", "RollingFile")
+				.newAppender(ERRORAPPENDER, "RollingFile")
 				.addAttribute("fileName",
-						"/opt/logs/jsp/error/error.log")
+                        FILEPATH + "/error/error.log")
 				.addAttribute("filePattern",
-						"/opt/logs/jsp/error/error-%d{yyyy-MM-dd}.log");
+                        FILEPATH + "/error/error-%d{yyyy-MM-dd}.log");
 		appenderBuilder.add(builder.newLayout("ThresholdFilter")
-				.addAttribute("level", "ERROR")
+				.addAttribute("level", Level.ERROR)
 				.addAttribute("onMatch", "ACCEPT")
 				.addAttribute("onMismatch", "DENY"));
 		appenderBuilder.add(builder.newLayout("PatternLayout").addAttribute(
@@ -140,30 +144,31 @@ public class CustomConfigurationFactory extends ConfigurationFactory{
 	private AppenderComponentBuilder getAsyncBuilder(
 			ConfigurationBuilder<BuiltConfiguration> builder) {
 		AppenderComponentBuilder appenderBuilder = builder
-				.newAppender("ASYNC", "Async");
+				.newAppender(ASYNC, "Async");
 		appenderBuilder.add(builder.newLayout("AppenderRef")
-					.addAttribute("ref", "myInfo"));
+					.addAttribute("ref", INFOAPPENDER));
 		appenderBuilder.add(builder.newLayout("AppenderRef")
-					.addAttribute("ref", "myError"));
+					.addAttribute("ref", ERRORAPPENDER));
 		return appenderBuilder;
 	}
 
 	/**
-	 * 添加开发环境的支持
+	 * 添加debug环境
 	 * @param builder
 	 */
 	private void addDevLogger(ConfigurationBuilder<BuiltConfiguration> builder){
-		// 添加mybatis支持
+		// 添加mybatis-debug支持
 		LoggerComponentBuilder loggerMyBatisBuilder = builder
-				.newLogger("com.pcbwx.jsp.dao", "debug")
+				.newLogger("com.pcbwx.jsp.dao", Level.DEBUG)
 				.addAttribute("additivity", false)
-				.add(builder.newAppenderRef("Console"));
+				.add(builder.newAppenderRef(CONSOLE));
 		builder.add(loggerMyBatisBuilder);
-		
+
+        // 添加spring-debug支持
 		LoggerComponentBuilder loggerSpringBuilder = builder
-				.newLogger("org.springframework", "INFO")
+				.newLogger("org.springframework", Level.INFO)
 				.addAttribute("additivity", false)
-				.add(builder.newAppenderRef("Console"));
+				.add(builder.newAppenderRef(CONSOLE));
 		builder.add(loggerSpringBuilder);
 	}
 }
--- src/test/java/heyu/HeyuTest.java
+++ src/test/java/heyu/HeyuTest.java
@@ -1,6 +1,9 @@
 package heyu;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.AppendersPlugin;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,6 +15,9 @@ public class HeyuTest {
 
 	public static void main(String[] args){
 		System.out.println(Boolean.valueOf("true"));
+
+        Level.INFO.name();
+        System.out.println( Level.INFO);
 	}
 
 }
